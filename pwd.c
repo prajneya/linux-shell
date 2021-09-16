48fb0c0c00728f2924df59b505b9474c7c995bca
Prajneya <prajneya.kumar@research.iiit.ac.in>
2021-09-16 16:19:24 +0530

    update pwd functionality

--- pwd.c
+++ pwd.c
@@ -9,34 +9,34 @@ void pwd_command(char *argv[]){
         char cwd[PATH_MAX] = { '\0' };
         getcwd(cwd, sizeof(cwd));
 
-        int ii;
-        for(ii = 0; ii<strlen(cwd) && ii<strlen(home_dir); ii++){
-            if(cwd[ii]!=home_dir[ii]){
-                break;
-            }
-        }
+        // int ii;
+        // for(ii = 0; ii<strlen(cwd) && ii<strlen(home_dir); ii++){
+        //     if(cwd[ii]!=home_dir[ii]){
+        //         break;
+        //     }
+        // }
         
-        int cwd_len = strlen(cwd);
-        int home_len = strlen(home_dir);
+        // int cwd_len = strlen(cwd);
+        // int home_len = strlen(home_dir);
 
-        if(cwd_len>=home_len){
-            char wd[PATH_MAX] = { '\0' };
-            int k = 0;
-            for(int j = ii; j<strlen(cwd) || k<strlen(wd); j++){
-                wd[k] = cwd[j];
-                k++;
-            }
-            if(k==0){
-                printf("~\n");
-            }
-            else{
-                printf("~%s\n", wd);
-            }
+        // if(cwd_len>=home_len){
+        //     char wd[PATH_MAX] = { '\0' };
+        //     int k = 0;
+        //     for(int j = ii; j<strlen(cwd) || k<strlen(wd); j++){
+        //         wd[k] = cwd[j];
+        //         k++;
+        //     }
+        //     if(k==0){
+        //         printf("~\n");
+        //     }
+        //     else{
+        //         printf("~%s\n", wd);
+        //     }
             
-        }
-        else{
+        // }
+        // else{
             printf("%s\n", cwd);
-        } 
+        // } 
     }   
 	return;
 }
